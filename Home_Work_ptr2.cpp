2aab3919df5f12af7fad85c5e020178fd2b85c7e
sadev <sadev@DESKTOP-5TK1OND>
2024-03-12 19:15:46 +0200

    Добавьте файлы проекта.

---
+++ Home_Work_ptr2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void exponent(int num1, int num2, int* result) {
+    *result = 1;
+    for (int i = 0; i < num2; i++) {
+        *result *= num1;
+    }
+}
+
+int main() {
+
+    int A, B;
+
+    cout << "Input A: ";
+    cin >> A;
+    cout << "Input B: ";
+    cin >> B;
+
+    int result;
+
+    exponent(A, B, &result);
+
+    cout << "A in B exponent equal: " << result << "\n";
+
+    return 0;
+}
